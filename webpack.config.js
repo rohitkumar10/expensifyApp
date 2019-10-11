8d0517898a19e80c51c25ca170eed3a0b33cf242
rohitkumar10 <rkjust15220041@gmail.com>
2019-10-11 19:49:08 +0530

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
@@ -20,9 +23,11 @@ module.exports = {
             ]
         }]
     },
-    devtool: 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
     devServer: {
         contentBase: path.join(__dirname, 'public'),
         historyApiFallback: true 
+     }
     }
 }
+    
